7eb99bee0cb65e9da294ec4d39baed644986e776
Nhật Minh <hoangnhatminh20042006@gmail.com>
2023-07-08 21:07:24 +0700

    ...

--- CGO_Buoi04/FindIndex/Program.cs
+++ CGO_Buoi04/FindIndex/Program.cs
@@ -3,6 +3,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        string[] students = {"Christian", "Michael", "Camila", "Sienna", "Tanya", "Connor", "Zachariah", "Mallory", "Zoe", "Emily"};
+        Console.WriteLine("Enter a name’s student:");
+        string input_name = Console.ReadLine();
+        bool isok = false;
+        /*
+        for(int i = 0; i < students.Length; i++){
+            if(students[i].Equals(input_name)){
+                isok = true;
+                Console.WriteLine("Position of the students in the list " + input_name + " is: " + (i + 1));
+                break;
+            }
+        }
+        if(!isok){
+            Console.WriteLine("Not found" + input_name + " in the list.");
+        }
+        */
+        List<string> list = new List<string>(students);
+        for(int i = 0; i < list.Count;i++){
+            if(list[i].Equals(input_name)){
+                isok = true;
+                Console.WriteLine("Position of the students in the list " + input_name + " is: " + (i + 1));
+                break;
+            }
+        }
+        if(!isok){
+            Console.WriteLine("Not found" + input_name + " in the list.");
+        }
+
+
     }
 }
