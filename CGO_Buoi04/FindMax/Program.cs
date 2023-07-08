0508499a13b834c1ee69e99a29f878e50ed19338
Nhật Minh <hoangnhatminh20042006@gmail.com>
2023-07-09 00:55:31 +0700

    ...

--- CGO_Buoi04/FindMax/Program.cs
+++ CGO_Buoi04/FindMax/Program.cs
@@ -3,6 +3,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<int> list = new List<int>();
+        int size;
+        do{
+            Console.WriteLine("Nhap size cua danh sach:");
+            size = Int32.Parse(Console.ReadLine());
+            if(size > 20){
+                Console.WriteLine("Khong duoc nhap qua 20 ");
+            }
+        }while (size >20);
+        for(int i = 0; i < size; i++){
+            Console.WriteLine("Nhap phan tu vi tri "+i+" trong danh sach");
+            list.Add(Int32.Parse(Console.ReadLine()));
+        }
+        int max = list.Max();
+        int index = list.IndexOf(max);
+        Console.WriteLine("vi tri cua so lon nhat la "+index+" va co gia tri la "+ max);
     }
 }
