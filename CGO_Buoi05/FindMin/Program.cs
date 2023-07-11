d5a870138913c9841dbd7776256ea81565389e8c
Nhật Minh <hoangnhatminh20042006@gmail.com>
2023-07-11 07:43:24 +0700

    ...

--- CGO_Buoi05/FindMin/Program.cs
+++ CGO_Buoi05/FindMin/Program.cs
@@ -1,8 +1,20 @@
 ﻿namespace FindMin;
 class Program
 {
+
+    static int MinValue(int[] array){
+        int min = array[0];
+        for(int i = 1; i < array.Length; i++){
+            if(array[i]< min){
+                min = array[i];
+            }
+        }
+        return min;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[]arr = {3,7,8,25,88,34,2,-235,2,62};
+        Console.WriteLine("Day so chung ta dang co la: {0} ",string.Join(" ,",arr));
+        Console.WriteLine("Gia tri be nhat trong day la: {0}", MinValue(arr));
     }
 }
