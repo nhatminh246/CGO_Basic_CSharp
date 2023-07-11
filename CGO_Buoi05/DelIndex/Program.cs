810bd4003c4ff96624c3c3bb32726aaea6eebf87
Nhật Minh <hoangnhatminh20042006@gmail.com>
2023-07-11 20:19:09 +0700

    zzz

--- CGO_Buoi05/DelIndex/Program.cs
+++ CGO_Buoi05/DelIndex/Program.cs
@@ -1,8 +1,41 @@
 ﻿namespace DelIndex;
 class Program
 {
+
+    static void DelIndex(int[] arr, int index){
+        for (int i = index; i < arr.Length-1; i++){
+            arr[i] = arr[i+1];
+        }
+        arr[arr.Length-1] = default(int);
+    }
+    static bool Check(int[] array, int value, ref int index){
+        for (int i = 0; i < array.Length;i++){
+            if (array[i] == value){
+                index = i;
+                return true;
+            }
+                
+        }
+        return false;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int index = -1, value;
+        bool flag = false;
+        int[] arr = { 1, 2,5,7,3,36};  
+        Console.Write("Mang hien tai co gia tri la: ");
+        Console.WriteLine(string.Join(",", arr));  
+        Console.WriteLine("Nhap gia tri can xoa");
+        value = Int32.Parse(Console.ReadLine());
+        flag = Check(arr, value, ref index);
+        if(flag){
+            if(index != - 1) Console.WriteLine("Gia tri ban muon xoa co trong mang va nam o vi tri {0}",index);
+            DelIndex(arr,index);
+            Console.Write("Mang sau khi xoa co gia tri la: ");
+            Console.WriteLine(string.Join(",", arr.Take(arr.Length -1)));
+        }else{
+            Console.WriteLine("Phan tu can xoa khong co trong mang");
+        }
+       
     }
 }
