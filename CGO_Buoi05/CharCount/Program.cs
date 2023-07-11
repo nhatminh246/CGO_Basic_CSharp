90d06e6b560c8b3cd61c0876b02c06a4cacb2b4e
Nhật Minh <hoangnhatminh20042006@gmail.com>
2023-07-11 20:31:29 +0700

    /\

--- CGO_Buoi05/CharCount/Program.cs
+++ CGO_Buoi05/CharCount/Program.cs
@@ -1,8 +1,23 @@
 ﻿namespace CharCount;
 class Program
 {
+
+    static int CharCount(string str, char c){
+        int index = 0;
+        for(int i = 0; i < str.Length; i++){
+            if(str[i] == c){
+                index++;
+            }
+        }
+        return index;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string s = "Day la 1 vi du de dem so luong ky tu theo yeu cau cua nguoi dung";
+        Console.WriteLine(s);
+        Console.WriteLine("Nhap ky tu ban muon dem tu chuoi o tren !");
+        char c = Console.ReadLine()[0];
+        Console.WriteLine("So luonng ky tu \"{0}\" co trong chuoi o tren la {1}", c,CharCount(s, c));
+
     }
 }
