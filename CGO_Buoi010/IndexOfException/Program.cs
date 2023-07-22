22a68063ced6264ac146dc28ee05ea0b012133e4
Nhật Minh <hoangnhatminh20042006@gmail.com>
2023-07-22 21:08:16 +0700

    ...

---
+++ CGO_Buoi010/IndexOfException/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace IndexOfException;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+            {
+                List<char> characters = new List<char>();
+                characters.InsertRange(0, new Char[] { 'n', 'h', 'a', 't', 'm', 'i','n','h' });
+                char value = characters[10];
+            }
+            catch (IndexOutOfRangeException  ex)
+            {
+                System.Console.WriteLine(ex);
+            }
+
+    }
+}
