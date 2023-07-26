f72eaf5ce51cedfae92c68501e0c0700bf1ee926
Nhật Minh <hoangnhatminh20042006@gmail.com>
2023-07-26 20:46:35 +0700

    f

---
+++ CGO_Buoi11/FileCopy/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace FileCopy;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try{
+            string source = "source.dll";
+            string des = "des.dll";
+            File.Copy(source, des,true);
+            Console.WriteLine("Copy completed");
+        }catch (IOException e)
+            {
+                Console.WriteLine("Cannot Copy");
+                Console.Error.WriteLine(e.Message);
+            }
+    }
+}
---
+++ CGO_Buoi11/Sum/Program.cs
@@ -0,0 +1,41 @@
+﻿﻿using System;
+using System.IO;
+
+namespace Sum
+{
+    class ReadTextFileExample
+    {
+        static void Main(string[] args)
+        {
+            string path = "test.txt";
+            ReadTextFileExample.ReadTextFile(path);
+        }
+        
+        static void ReadTextFile(string filePath)
+        {
+            try
+            {
+                FileInfo file = new FileInfo(filePath);
+                if (!file.Exists)
+                {
+                    throw new FileNotFoundException();
+                }
+
+                using(StreamReader r = new StreamReader(filePath)){
+                string line = "";
+                int sum = 0;
+                while((line = r.ReadLine()) != null) 
+                {
+                    Console.WriteLine(line);
+                    sum += Int32.Parse(line);
+                }
+                Console.WriteLine("Tổng: " + sum);
+                }
+            }
+            catch (System.Exception)
+            {
+                Console.Error.WriteLine("K có file hoặc nội dung không hợp lệ");
+            }
+        }
+    }
+}
